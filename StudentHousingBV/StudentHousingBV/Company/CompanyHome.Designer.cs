122a22cbcbaf7c9fccfd803396cb7dcb847e3a16
Bogdanov,Alexander A.T <a.bogdanov@student.fontys.nl>
2020-12-07 18:50:22 +0100

    Login Page Design Completed

---
+++ StudentHousingBV/StudentHousingBV/Company/CompanyHome.Designer.cs
@@ -0,0 +1,294 @@
+﻿
+namespace StudentHousingBV
+{
+    partial class CompanyHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CompanyHome));
+            this.label14 = new System.Windows.Forms.Label();
+            this.lblStudentMonth = new System.Windows.Forms.Label();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblLogOut = new System.Windows.Forms.Label();
+            this.lblAccount = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.lblSHBV = new System.Windows.Forms.Label();
+            this.lblTenants = new System.Windows.Forms.Label();
+            this.lblComplaints = new System.Windows.Forms.Label();
+            this.lblAnnouncements = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblRules = new System.Windows.Forms.Label();
+            this.lblDescription = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(738, 168);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(129, 17);
+            this.label14.TabIndex = 11;
+            this.label14.Text = "<PUTNAMEHERE>";
+            // 
+            // lblStudentMonth
+            // 
+            this.lblStudentMonth.AutoSize = true;
+            this.lblStudentMonth.Location = new System.Drawing.Point(710, 151);
+            this.lblStudentMonth.Name = "lblStudentMonth";
+            this.lblStudentMonth.Size = new System.Drawing.Size(184, 17);
+            this.lblStudentMonth.TabIndex = 10;
+            this.lblStudentMonth.Text = "STUDENT OF THE MONTH";
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblWelcome.Location = new System.Drawing.Point(466, 97);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(129, 29);
+            this.lblWelcome.TabIndex = 9;
+            this.lblWelcome.Text = "Welcome!";
+            // 
+            // pbImage
+            // 
+            this.pbImage.Image = ((System.Drawing.Image)(resources.GetObject("pbImage.Image")));
+            this.pbImage.Location = new System.Drawing.Point(79, 138);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(361, 228);
+            this.pbImage.TabIndex = 8;
+            this.pbImage.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.panel1.Controls.Add(this.lblLogOut);
+            this.panel1.Controls.Add(this.lblAccount);
+            this.panel1.Controls.Add(this.label11);
+            this.panel1.Controls.Add(this.lblSHBV);
+            this.panel1.Controls.Add(this.lblTenants);
+            this.panel1.Controls.Add(this.lblComplaints);
+            this.panel1.Controls.Add(this.lblAnnouncements);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Location = new System.Drawing.Point(-1, -3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1209, 78);
+            this.panel1.TabIndex = 7;
+            // 
+            // lblLogOut
+            // 
+            this.lblLogOut.AutoSize = true;
+            this.lblLogOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblLogOut.Location = new System.Drawing.Point(1096, 30);
+            this.lblLogOut.Name = "lblLogOut";
+            this.lblLogOut.Size = new System.Drawing.Size(85, 20);
+            this.lblLogOut.TabIndex = 10;
+            this.lblLogOut.Text = "LOG OUT";
+            this.lblLogOut.Click += new System.EventHandler(this.lblLogOut_Click);
+            // 
+            // lblAccount
+            // 
+            this.lblAccount.AutoSize = true;
+            this.lblAccount.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblAccount.Location = new System.Drawing.Point(918, 30);
+            this.lblAccount.Name = "lblAccount";
+            this.lblAccount.Size = new System.Drawing.Size(91, 20);
+            this.lblAccount.TabIndex = 8;
+            this.lblAccount.Text = "ACCOUNT";
+            this.lblAccount.Click += new System.EventHandler(this.lblAccount_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Yi Baiti", 72F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(1015, -30);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(79, 120);
+            this.label11.TabIndex = 9;
+            this.label11.Text = "|";
+            // 
+            // lblSHBV
+            // 
+            this.lblSHBV.AutoSize = true;
+            this.lblSHBV.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblSHBV.Location = new System.Drawing.Point(13, 30);
+            this.lblSHBV.Name = "lblSHBV";
+            this.lblSHBV.Size = new System.Drawing.Size(171, 40);
+            this.lblSHBV.TabIndex = 0;
+            this.lblSHBV.Text = "STUDENT HOUSING\r\n              B.V.\r\n";
+            // 
+            // lblTenants
+            // 
+            this.lblTenants.AutoSize = true;
+            this.lblTenants.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblTenants.Location = new System.Drawing.Point(710, 30);
+            this.lblTenants.Name = "lblTenants";
+            this.lblTenants.Size = new System.Drawing.Size(86, 20);
+            this.lblTenants.TabIndex = 6;
+            this.lblTenants.Text = "TENANTS";
+            this.lblTenants.Click += new System.EventHandler(this.lblTenants_Click);
+            // 
+            // lblComplaints
+            // 
+            this.lblComplaints.AutoSize = true;
+            this.lblComplaints.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblComplaints.Location = new System.Drawing.Point(273, 30);
+            this.lblComplaints.Name = "lblComplaints";
+            this.lblComplaints.Size = new System.Drawing.Size(117, 20);
+            this.lblComplaints.TabIndex = 2;
+            this.lblComplaints.Text = "COMPLAINTS";
+            this.lblComplaints.Click += new System.EventHandler(this.lblComplaints_Click);
+            // 
+            // lblAnnouncements
+            // 
+            this.lblAnnouncements.AutoSize = true;
+            this.lblAnnouncements.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblAnnouncements.Location = new System.Drawing.Point(468, 30);
+            this.lblAnnouncements.Name = "lblAnnouncements";
+            this.lblAnnouncements.Size = new System.Drawing.Size(162, 20);
+            this.lblAnnouncements.TabIndex = 4;
+            this.lblAnnouncements.Text = "ANNOUNCEMENTS";
+            this.lblAnnouncements.Click += new System.EventHandler(this.lblAnnouncements_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Yi Baiti", 72F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(816, -30);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(79, 120);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "|";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Yi Baiti", 72F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(608, -30);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 120);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "|";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Yi Baiti", 72F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(178, -30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 120);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "|";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Yi Baiti", 72F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(396, -30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 120);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "|";
+            // 
+            // lblRules
+            // 
+            this.lblRules.AutoSize = true;
+            this.lblRules.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblRules.ForeColor = System.Drawing.Color.Red;
+            this.lblRules.Location = new System.Drawing.Point(1094, 507);
+            this.lblRules.Name = "lblRules";
+            this.lblRules.Size = new System.Drawing.Size(82, 25);
+            this.lblRules.TabIndex = 12;
+            this.lblRules.Text = "RULES";
+            this.lblRules.Click += new System.EventHandler(this.lblRules_Click);
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(76, 401);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(491, 51);
+            this.lblDescription.TabIndex = 13;
+            this.lblDescription.Text = "Student Housing B.V. is a housing company with over 2.500 acommodations \r\nthrough" +
+    "out the Netherlands. We provide quality services, studios with their\r\nown bathro" +
+    "oms and shared kitchens.\r\n";
+            // 
+            // CompanyHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1198, 552);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.lblRules);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.lblStudentMonth);
+            this.Controls.Add(this.lblWelcome);
+            this.Controls.Add(this.pbImage);
+            this.Controls.Add(this.panel1);
+            this.Name = "CompanyHome";
+            this.Text = "CompanyHome";
+            this.Load += new System.EventHandler(this.CompanyHome_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label lblStudentMonth;
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblSHBV;
+        private System.Windows.Forms.Label lblAccount;
+        private System.Windows.Forms.Label lblTenants;
+        private System.Windows.Forms.Label lblComplaints;
+        private System.Windows.Forms.Label lblAnnouncements;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblLogOut;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label lblRules;
+        private System.Windows.Forms.Label lblDescription;
+    }
+}
